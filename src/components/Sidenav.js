b84f419051690f2a706c6e6649f6b43772fd4c45
Abubakarshykh <sheikhabubaker498@gmail.com>
2023-05-09 15:10:58 +0500

    Update Sidenav.js

--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -9,22 +9,31 @@ import Image from 'next/image'
   return (
    <>
     <div className='flex'>
-    <button data-drawer-target='separator-sidebar' data-drawer-toggle='separator-sidebar' aria-controls='separator-sidebar' type='button' className='inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600'>
+    <button 
+    data-drawer-target='separator-sidebar' 
+    data-drawer-toggle='separator-sidebar' 
+    aria-controls='separator-sidebar' 
+    type='button' 
+    className='inline-flex items-center p-2 mt-2 ml-3 text-sm text-gray-500 rounded-lg 
+    md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200'>
     <span className='sr-only'>Open sidebar</span>
     <svg className='w-6 h-6' aria-hidden='true' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
-       <path clip-rule='evenodd' fill-rule='evenodd' d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z'></path>
+       <path 
+       clip-rule='evenodd' 
+       fill-rule='evenodd'
+        d='M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z'></path>
     </svg>
  </button>
  
- <aside id='separator-sidebar' className='fixed top-0 left-0 z-40 w-[17rem] h-screen transition-transform -translate-x-full sm:translate-x-0' aria-label='Sidebar'>
+ <aside id='separator-sidebar' className='fixed top-0 left-0 z-40 md:w-[17rem] h-screen transition-transform -translate-x-full sm:translate-x-0' aria-label='Sidebar'>
     <div className='h-screen sm:h-screen px-3 overflow-y-auto bg-[#0d44b3]'>
-        <span className='flex py-1'>
+        <span className='flex py-2'>
             <Image 
             src='/images/Group 5.png' 
             className=' text-white ml-5 w-6 h-6 my-2' 
             width={30} height={30} 
             alt=''/>
-            <Link href='/Overview' className='text-white  flex text-md font-medium px-2 pt-2'>CROWD<p className='text-white font-light text-md '>WRK</p></Link>
+            <Link href='/Overview' className='text-white  flex text-lg font-medium px-2 pt-2'>CROWD<p className='text-white font-light text-lg '>WRK</p></Link>
         </span>
        <ul className='space-y-1 font-medium lg:pl-4 lg:pr-4'>
           <li>
@@ -75,7 +84,7 @@ import Image from 'next/image'
              </Link>
           </li>
        </ul>
-       <ul className='pt-6 space-y-1 font-medium lg:pl-4 lg:pr-4'>
+       <ul className='pt-10 space-y-1 font-medium lg:pl-4 lg:pr-4'>
           <li>
              <Link href='#' className='flex items-center p-2 text-gray-900 transition duration-75 rounded-lg hover:bg-[#2f68d6] dark:hover:bg-gray-700 dark:text-white group'>
         
@@ -96,12 +105,12 @@ import Image from 'next/image'
              </Link>
           </li>
           <li className='text-white font-normal '>
-          <div className='flex text-white gap-3 border-t mt-3 pt-2'>
-            <Image src='/images/Avatar.png' alt='' width={20} height={20} className= 'w-8 h-8 ml-3 mt-3'/>
-            <Link href={'#'} className='pt-3 text-lg font-medium'>  Olivia Rhye</Link> 
+          <div className='flex text-white gap-3 border-t lg:mt-5 pt-2'>
+            <Image src='/images/Avatar.png' alt='' width={20} height={20} className= 'w-8 h-8 ml-2 mt-3'/>
+            <Link href={'/'} className='pt-3 text-lg font-medium'>  Olivia Rhye</Link> 
             <Image src='/images/shareicon.png' width={20} height={20} alt='' className='cursor-pointer text-slate-600 w-5 h-5 mt-4 ml-auto'/>
           </div>
-          <Link href={'#'} className='pl-[3.5rem] font-light text-lg text-gray-100'>olivia@email.com</Link>
+          <Link href={'/'} className='pl-[3.3rem] font-light text-lg text-gray-100'>olivia@email.com</Link>
           </li>
        </ul>
     </div>
